b4b2372a9cdc624b91b3f2d10500fdc426974aef
junghun bak <wjdgnsi79@naver.com>
2022-12-08 22:12:42 +0900

    step1

--- Study_4/ViewController.swift
+++ Study_4/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animetionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func animateBtnDidTouch(_ sender:Any) {
+        UIView.animate(withDuration: 1, delay: 0.5 , animations: {
+            self.animetionView.backgroundColor = .systemPink
+            self.animetionView.frame.size.width += 10
+            self.animetionView.frame.size.height += 10
+        }) {_ in
+            UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn) {
+                self.animetionView.backgroundColor = .orange
+                self.animetionView.frame.origin.y += 40
+            }
+        }
+    }
+    
+   
 
 }
 
